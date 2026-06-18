ba34e24b39f301a76629abc067fdc5dc63381eb0
hamooda12 <tourt2031@gmail.com>
2026-06-18 10:04:05 +0300

    Add docker file

---
+++ src/main/java/org/example/aleasofttrainner/loadData.java
@@ -0,0 +1,40 @@
+package org.example.aleasofttrainner;
+
+import org.example.aleasofttrainner.Security.AppUser;
+import org.example.aleasofttrainner.Security.AppUserRepository;
+import org.example.aleasofttrainner.Security.Role;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.math.BigDecimal;
+import java.util.Random;
+
+@Configuration
+public class loadData {
+    @Bean
+    CommandLineRunner seedData(
+                               AppUserRepository appUserRepository,
+                               PasswordEncoder passwordEncoder) {
+        return args -> {
+
+            if (appUserRepository.count() == 0) {
+                appUserRepository.save(AppUser.builder()
+                        .email("admin@hotel.local")
+                        .password(passwordEncoder.encode("Admin@123"))
+                        .role(Role.ADMIN)
+                        .build());
+
+                appUserRepository.save(AppUser.builder()
+                        .email("guest@hotel.local")
+                        .password(passwordEncoder.encode("Guest@123"))
+                        .role(Role.GUEST)
+                        .build());
+
+            }
+
+
+        }
+;
+    }}
