23ca265fb0dc70d237657ea6fb99db9f8e886c93
hamooda12 <tourt2031@gmail.com>
2026-06-28 12:00:45 +0300

    fix: edit the path above

--- src/main/java/org/example/aleasofttrainner/HealthController.java
+++ src/main/java/org/example/aleasofttrainner/HealthController.java
@@ -1,3 +1,5 @@
+package org.example.aleasofttrainner;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
